a4cb3dcdd22ee2827771282c101e3fc3aceae783
Brian Mutinda <mutindabrian26@gmail.com>
2020-02-07 10:42:21 +0300

    finish login form layout

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,43 @@
 <template>
     <div class="row">
-      <div class="col-xs-12 col-md-6 offset-md-3 ">
-        <q-card dark>
-          <q-img src=""></q-img>
-        </q-card>
+      <div class="col-xs-12 col-sm-4 offset-sm-4 ">
+        <q-card class="q-mt-xl">
+          <center>
+            <q-avatar size="150px">
+              <q-img src="statics/images/person.png"></q-img>
+            </q-avatar>
+          </center>
 
-        <q-card-section>
-          <q-input>
-            <template v-slot: prepend>
-              <q-icon name="event"></q-icon>
-            </template>
-          </q-input>
-        </q-card-section>
+          <q-card-section class="q-px-xl">
+            <q-input color="purple-12" v-model="text" label="Email">
+              <template v-slot:prepend>
+                <q-icon name="person" />
+              </template>
+            </q-input>
+
+            <br>
+
+            <q-input color="purple-12" v-model="text" label="Password">
+              <template v-slot:prepend>
+                <q-icon name="lock" />
+              </template>
+            </q-input>
+
+            <br>
+            <center>
+              <q-btn color="primary" size='md'>Submit</q-btn>
+            </center>
+
+          </q-card-section>
+
+        </q-card>
       </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "loginComponent"
+        name: "loginComponent",
     }
 </script>
 
