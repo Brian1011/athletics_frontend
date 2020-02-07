d812080e1a56dafb90dd8ce166642e7c777447cd
Brian Mutinda <mutindabrian26@gmail.com>
2020-02-07 14:30:27 +0300

    add validate js

---
+++ src/boot/validate.js
@@ -0,0 +1,17 @@
+// // import something here
+// // import Vue from 'vue';
+// import VeeValidate from 'vee-validate';
+//
+// // Vue.use(VeeValidate);
+//
+// export default async ({Vue})=>{
+//   Vue.use(VeeValidate)
+// }
+// // export default ({Vue})=>{
+// //   Vue.use(VeeValidate)
+// // }
+//
+// // // "async" is optional
+// // export default async ({ /* app, router, Vue, ... */ }) => {
+// //   // something to do
+// // }
