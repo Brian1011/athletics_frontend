e56447d65043beb40e5455f3a870a22ee7e9a94e
Brian Mutinda <mutindabrian26@gmail.com>
2020-02-10 12:09:31 +0300

    add colour gradient to cards

--- src/components/dashboard/SummaryCards.vue
+++ src/components/dashboard/SummaryCards.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="row q-px-lg q-py-lg">
       <div class="col-4">
-        <q-card class="my-card bg-purple text-white q-mr-sm">
+        <q-card class="my-card text-white q-mr-sm summary_card_success">
           <q-card-section>
             <div class="text-h6">Users</div>
             <q-separator dark/>
@@ -23,7 +23,7 @@
       </div>
 
       <div class="col-4">
-        <q-card class="my-card bg-purple text-white q-mr-sm">
+        <q-card class="my-card text-white q-mr-sm summary_card_success">
           <q-card-section>
             <div class="text-h6">Sales</div>
             <q-separator dark/>
@@ -44,7 +44,7 @@
       </div>
 
       <div class="col-4">
-        <q-card class="my-card bg-purple text-white q-mr-sm">
+        <q-card class="my-card text-white q-mr-sm summary_card_failure">
           <q-card-section>
             <div class="text-h6">Profits</div>
             <q-separator dark/>
@@ -53,10 +53,10 @@
             <div class="row">
               <div class="col-6">
                 <div class="text-h2">2000</div>
-                <div class="text-subtitle2">Today's Profit</div>
+                <div class="text-subtitle2">Today's Profit (Ksh)</div>
               </div>
               <div class="col-6">
-                <div class="text-h5"><q-icon name="arrow_downward" size="md" class="text-black"></q-icon>12.38% </div>
+                <div class="text-h5"><q-icon name="arrow_downward" size="md" class="text-orange"></q-icon>12.38% </div>
               </div>
             </div>
 
@@ -74,5 +74,10 @@
 </script>
 
 <style scoped>
-
+  .summary_card_success{
+    background: linear-gradient(to bottom right, #9C27B0 0%, #009900 100%);
+  }
+  .summary_card_failure{
+    background: linear-gradient(to bottom right, #9C27B0 0%, red 100%);
+  }
 </style>
